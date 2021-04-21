79d32bd9dfb8f75c8223049e9e6758350cf32bed
isenkovska <isenkovska@gmail.com>
2021-04-21 19:33:07 -0400

    user create. Then catch finally

--- src/modules/user/userRegister.js
+++ src/modules/user/userRegister.js
@@ -1,3 +1,21 @@
+import User from './Model';
+
 export default function userRegister(req, res) {
-  res.status(200).json('userRegister here');
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(() => {
+      res.status(200).json('User created');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json('User not created');
+    })
+    .finally(() => {
+      console.log('END');
+    });
 }
