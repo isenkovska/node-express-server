e019899c26888937379f29ebe2ee70217f77d974
isenkovska <isenkovska@gmail.com>
2021-04-22 14:14:10 -0400

    Filter, limit, skip

--- src/modules/user/userGetAll.js
+++ src/modules/user/userGetAll.js
@@ -2,6 +2,8 @@ import User from './Model';
 
 export default function userGetAll(req, res) {
   User.find()
+    .limit(2)
+    .skip(2)
     .exec()
     .then((result) => {
       res.status(200).json(result);
